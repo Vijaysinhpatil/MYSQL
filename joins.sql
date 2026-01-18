9de3b25a7e5458d668d5df629002de3f1e790ba9
vijaysinh Patil <vijaysinhpatil28@gmail.com>
2026-01-19 04:20:22 +0530

    joins and views

--- joins.sql
+++ joins.sql
@@ -54,3 +54,65 @@ select *
 from customer_details c
 right join order_details o
 on c.cust_id = o.cust_id;
+
+
+-- exclusive join
+select *
+from customer_details as a
+left join order_details as b
+on a.cust_id = b.cust_id
+where b.cust_id is null;
+
+-- exclusive join
+select * 
+from customer_details as a
+right join order_details as b
+on a.cust_id = b.cust_id
+where a.cust_id is null;
+
+-- subqueries
+SELECT * FROM 
+order_details 
+where amount > (
+    select avg(amount)
+    from order_details
+);
+-- subqueries with select
+
+select name,
+(
+    select count(*)
+    from order_details o
+    where o.cust_id = c.cust_id
+) as order_count
+from customer_details c;
+
+-- subquery with from
+select  
+      summary.cust_id , 
+      summary.avg_amount
+from
+	(
+	   select 
+             cust_id , 
+             avg(amount) as avg_amount
+	   from order_details
+	   group by cust_id
+	) as summary;
+
+-- creating a view
+
+create view view1 as
+select cust_id , name from customer_details;
+
+select * from view1;
+
+create view view2 as
+select c.cust_id , c.name , o.order_id
+from customer_details c
+inner join order_details o 
+on c.cust_id = o.cust_id;
+
+select * from view2;
+
+drop view view1;
